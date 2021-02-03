9c397d2ecb95c6a1ab10eb2043ae8c70a5379ff4
cskez <cskeza138@gmail.com>
2021-02-03 15:54:51 -0600

    Debut du projet

---
+++ test.py
@@ -0,0 +1,15 @@
+a= 250
+
+while True:
+     entree= input ("Entrez un nombre entre 1 et 1000 puis entree, faites 'q' our quitter")
+
+     if entree== 'q' :
+         break
+
+
+
+     elif entree < a:
+         print ("Plus grand.")
+         continue
+
+
